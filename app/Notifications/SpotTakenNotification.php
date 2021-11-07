827117ad19d9101741793e73e169547dc210a8da
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2021-11-07 11:19:46 +0100

    missing import

--- app/Notifications/SpotTakenNotification.php
+++ app/Notifications/SpotTakenNotification.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
+use NotificationChannels\Telegram\TelegramChannel;
 use NotificationChannels\Telegram\TelegramMessage;
 
 class SpotTakenNotification extends Notification implements ShouldQueue
