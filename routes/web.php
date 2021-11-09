7b4aab9872e2d26bd4d9a4a4f245536fa657aca6
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2021-11-09 16:06:22 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::get('/chargepoints/{chargePoint:clever_id}', function (Request $request, 
 });
 
 Route::get('/locations/{location}', function (Request $request, Location $location) {
-    dd($location->with(['availability', 'subscribers', 'chargePoints', 'connectors'])->get());
+    dd($location->load('availability', 'subscribers', 'chargePoints', 'connectors'));
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
